0b0e3d54f72ec86dc1dfb03d29e2f0095d02b996
abuttle2 <alexbuttle@hotmail.co.uk>
2023-02-02 21:12:55 +0000

    Added TMDB API call to log movie genres

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,20 @@
 $("#submitBtn").on("click", function (event) {
     event.preventDefault();
-    
+
     var city = $("#cityInput").val();
     var newSrc = "https://www.google.com/maps/embed/v1/search?q=campsite+" + city + "&key=AIzaSyAdjP3RyKMzcagNwl7pXD76vnw9KXjmTc0";
     $("#maps-iframe").attr("src", newSrc);
 });
+
+
+var searchMovies = function () {
+    // var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=0b443d2c2139bab3c9172850e7125437&language=en-US&query=horror&page=1&include_adult=false";
+    var queryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=0b443d2c2139bab3c9172850e7125437&language=en-US";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+}
+searchMovies();
