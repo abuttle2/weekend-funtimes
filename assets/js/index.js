8b03e4bacb969fef0d2d12c0652321d299bfa1d7
abuttle2 <alexbuttle@hotmail.co.uk>
2023-02-03 18:48:50 +0000

    Stored the genres into an array to use for the URL query

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,15 +6,41 @@ $("#submitBtn").on("click", function (event) {
     $("#maps-iframe").attr("src", newSrc);
 });
 
-
-var searchMovies = function () {
+var getMovieGenres = function () {
     // var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=0b443d2c2139bab3c9172850e7125437&language=en-US&query=horror&page=1&include_adult=false";
-    var queryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=0b443d2c2139bab3c9172850e7125437&language=en-US";
+    //Store movies objects into an array
+    var movieGenres = [];
+    var genreQueryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=0b443d2c2139bab3c9172850e7125437&language=en-US";
+
     $.ajax({
-        url: queryURL,
+        url: genreQueryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        for (var i = 0; i < response.genres.length; i++) {
+            movieGenres.push(response.genres[i]);
+        }
+        searchMovieID(movieGenres);
     });
 }
-searchMovies();
+
+var searchMovieID = function (genres) {
+    //Reference the movie genres objects
+
+    //Iterate through the array of genres objects
+    for (var i = 0; i < genres.length; i++) {
+        console.log(genres[i]);
+    }
+
+    var genreID = 27;
+    var queryURL2 = "https://api.themoviedb.org/3/discover/movie?api_key=0b443d2c2139bab3c9172850e7125437&with_genres=" + genreID;
+
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    }).then(function (response) {
+        // console.log(genres);
+    });
+
+}
+
+getMovieGenres();
