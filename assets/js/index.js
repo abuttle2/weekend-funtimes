8dd0b29068fb535bf3b418478795b4504395dd54
wilguille <willyguille97@gmail.com>
2023-02-03 18:17:32 +0000

    Made ajax call for openweather api and log result to console

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,4 +7,14 @@ $("#submitBtn").on("click", function (event) {
     // Generate the new src URL for the iframe
     var newSrc = "https://www.google.com/maps/embed/v1/search?q=campsite+" + city + "&key=AIzaSyAdjP3RyKMzcagNwl7pXD76vnw9KXjmTc0";
     $("#maps-iframe").attr("src", newSrc);
+
+    // Make an AJAX call to a different API
+    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&q=" + city + "&appid=32306d8e68f68c9295a794f157aaab66";
+    $.ajax({
+        url: apiURL,
+        success: function (data) {
+            console.log(data);
+            // Do something with the data
+        }
+    });
 });
