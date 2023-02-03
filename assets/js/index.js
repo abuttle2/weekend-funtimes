ce734634821d3dc387808c1dd48cabbb8bae5105
wilguille <willyguille97@gmail.com>
2023-02-03 18:02:55 +0000

    Added comments to previous code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
+// Listen for click event on the submit button
 $("#submitBtn").on("click", function (event) {
+    // Prevent the default form submit behavior
     event.preventDefault();
-    
+    // Get the value of the city input
     var city = $("#cityInput").val();
+    // Generate the new src URL for the iframe
     var newSrc = "https://www.google.com/maps/embed/v1/search?q=campsite+" + city + "&key=AIzaSyAdjP3RyKMzcagNwl7pXD76vnw9KXjmTc0";
     $("#maps-iframe").attr("src", newSrc);
 });
